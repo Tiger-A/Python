17b2386fb8d053a24b91dcf074eb54a46ba56e21
Alexei Efremov <td-interface@yandex.ru>
2022-10-16 15:39:41 +0400

    HW4 done

--- HW4.py
+++ HW4.py
@@ -34,16 +34,45 @@
 # Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0      или x² + 5 = 0 или     10*x² = 0
-import random
-natural_degree = int(input('Input natural degree - '))
 
-with open('file.txt', 'w') as data:
- data.write('line 1\n')
- data.write('line 2\n')
-data.close()
 
-r = random.randint(0,100)
-
+# import random
+# natural_degree = int(input('Input natural degree - '))
+# rezult = ""
+# for i in range(natural_degree, -1, -1):
+#     koeficent = str(random.randint(0,100))
+#     print(i,koeficent)
+#     if koeficent != 0:
+#         if i == 1:
+#             rezult = rezult + str(koeficent)+ "*X" + "+"  
+#         elif i == 0:
+#             rezult = rezult + str(koeficent) + "=0"  
+#         else: 
+#             rezult = rezult + str(koeficent)+ "*X**" + str(i) + "+"
+   
+#     print(rezult)
+# with open('file.txt', 'w') as data:
+#  data.write(rezult)
+#  data.close()
 
 
 # Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
+
+def write_file(name = '', mnogochlen= ""):
+    with open(name, 'w') as data:
+        data.write(mnogochlen) 
+        data.close()
+
+def read_file(name = ''):
+    with open(name,'r') as file:
+         mn_r = file.readline()
+         return mn_r
+
+write_file('file1.txt', '65*X**6+15*X**5+38*X**4+59*X**3+46*X**2+53*X+13=0')
+write_file('file2.txt', '69*X**4+94*X**3+97*X**2+65*X+73=0')
+mnogoclen1 = read_file('file1.txt')[:-2]
+mnogoclen2 = read_file('file2.txt')[:-2]
+sum_mnogochlenov = mnogoclen1 + "+" + mnogoclen2 + "=0"
+write_file("sum_mnogochlenov.txt", sum_mnogochlenov)
+
+
