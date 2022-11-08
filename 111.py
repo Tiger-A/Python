775a991c69a680d77d26d1da1a4283af8e56d6d3
Alexei Efremov <td-interface@yandex.ru>
2022-11-08 08:43:07 +0400

    added programs for TG

--- 111.py
+++ 111.py
@@ -2763,6 +2763,99 @@ app.add_handler(CommandHandler("new_game", new_game))
 app.add_handler(CommandHandler("game", game))
 app.run_polling()
 
+ 
+
+
+
+from config import TOKEN
+import requests
+import logging
+APScheduler==3.6.3
+cachetools==4.2.2
+certifi==2022.9.24
+charset-normalizer==2.1.1
+idna==3.4
+isOdd==0.1.2
+python-telegram-bot==13.12
+pytz==2022.4
+pytz-deprecation-shim==0.1.0.post0
+requests==2.28.1
+six==1.16.0
+tornado==6.2
+tzdata==2022.4
+tzlocal==4.2
+urllib3==1.26.12
+
+
+
+pip install -r requirements.txt
+
+
+
+import requests
+import logging
+from config import TOKEN
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+)
+
+
+# from dotenv import 
+# load_dotenv, find_dotenv
+# load_dotenv(find_dotenv())  другой способ хранения ключа не в программе папка .env добавляется в gitignor
+
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+update.message.text)
+
+logger.info("Пользователь ввел число: %s: %s", user.first_name, update.message.text)
+if __name__ == '__main__':
+
+
+
+# Определяем константы этапов разговора
+CHOICE, RATIONAL_ONE, RATIONAL, RATIONAL_TWO, OPERATIONS_RATIONAL, OPERATIONS_COMPLEX, COMPLEX_ONE, COMPLEX_TWO = range(8)
+    
+
+# функция обратного вызова точки входа в разговор
+
+API_URL = 'https://7015.deeppavlov.ai/model'
+
+
+
+# async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+
+# app.add_handler(CommandHandler("start", help))
+
+
+dispatcher = updater.dispatcher
+dispatcher.add_handler(conversation_handler)
+
+
+
+
+# from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+# from telegram import Update   другие библиотеки
+
+pip install python-telegram-bot --pre
+
+python-telegram-bot==20.0a4
+
+pip install python-telegram-bot --upgrade
+
+app = ApplicationBuilder().token(TOKEN).build()
 
 
 
@@ -2770,5 +2863,236 @@ app.run_polling()
 
 
 
+import requests
+import logging
+from config import TOKEN
+# from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update
 
+
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    ConversationHandler,
+)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+# logger.info("Пользователь ввел число: %s: %s", user.first_name, update.message.text)
+
+CHOICE, RATIONAL_ONE, RATIONAL, RATIONAL_TWO, OPERATIONS_RATIONAL, OPERATIONS_COMPLEX, COMPLEX_ONE, COMPLEX_TWO = range(8)
+API_URL = 'https://7015.deeppavlov.ai/model'
+
+def start():
+    print('hello')
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    
+async def ask_wikipedia_question(question):
+    API_URL = 'https://7012.deeppavlov.ai/model'
+    data = {'question_raw':  [question]}
+    res = requests.post(API_URL, json=data, verify=False).json()
+    return res[0][0]
+
+async def request_sentiment(message):
+    data = {'x': [message]}
+    res = requests.post(API_URL, json=data).json()
+    santiment = res[0][0]
+    # return santiment
+    await Update.message.reply_text(santiment)
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+
+
+app.add_handler(CommandHandler("start", help))
+app.add_handler(CommandHandler("sentiment", request_sentiment))
+app.run_polling()
+
+# dispatcher = updater.dispatcher
+# dispatcher.add_handler(conversation_handler)
+
+
+# if __name__ == '__main__':
+
+
+
+
+
+# Создайте программу для игры в ""Крестики-нолики"".
+
+import telebot
+from telebot import types
+
+TOKEN = "5501756584:AAEw3bT6QibRgjGBP1enKb5diEZXmRVMfmU"
+bot = telebot.TeleBot(TOKEN)
+
+pole = list(range(1, 10))
+player = 'X'
+win_komb = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
+            (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+
+@bot.message_handler(commands=['start'])
+def bot_pole(message):
+    bot.send_message(message.chat.id, draw_pole() )
+    
+    
+
+@bot.message_handler(content_types=['text'])
+def bot_message(message, pole=pole):
+    global player
+    hod = message.text
+    if  hod not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        bot.send_message(message.chat.id,'Введите верный номер клеточки для хода')
+    else:
+        hod = int(hod)
+        if str(pole[hod-1]) in 'XO':
+            bot.send_message(message.chat.id, 'Клетка занята, выберите другую')
+        else:
+            pole[hod-1] = player
+            bot.send_message(message.chat.id, draw_pole() )
+            if player == 'X':
+                player = 'O'
+            else:
+                player = 'X'
+
+
+def draw_pole(): # функция отрисовки игрового поля
+    new_pole = '-------------\n'
+    for i in range(3):
+        new_pole += '| ' + str(pole[0 + i*3]) + ' | ' +  str(pole[1 + i*3]) + ' | ' + str(pole[2 + i*3]) + ' |\n'
+    new_pole +='-------------'
+    return new_pole
+
+def take_hod(hod, pole=pole): # фукнция хода игрока
+    while True:
+        # hod = input('Куда поставить ' + player + ' ? ')
+        if  hod not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            # print('Введите вереый номер клеточки для хода')
+            continue
+        hod = int(hod)
+        if str(pole[hod-1]) in 'XO':
+            # print('Клетка занята, выберите другую')
+            continue
+        pole[hod-1] = player
+        break
+    
+
+
+def check_win(): # функция проверка на выигрыш
+    for elem in win_komb:
+        if pole[elem[0]] == pole[elem[1]] == pole[elem[2]]:
+            return pole[elem[0]]
+    else:
+        return False
+
+# def game(): # функция самой игры и смены ходов
+#     a = 0
+#     while True:
+#         if a % 2 == 0: 
+#             bot_message()
+#             take_hod('X')
+#         else:
+#             take_hod('O')
+#         if a > 3:
+#             win = check_win()
+#             if win:
+#                 # os.system('cls')
+#                 draw_pole()
+#                 print(win, 'Выиграл!!!')
+#                 break
+#         a += 1
+#         if a > 8:
+#             # os.system('cls')
+#             draw_pole()
+#             print('Ничья!!')
+#             break
+# game()
+
+bot.polling()
+
+
+
+
+https://github.com/BlackStoneShadow/Python/tree/main/Lesson10/Task1
+
+
+
+
+
+import telebot
+from telebot import types
+
+TOKEN = " "
+bot = telebot.TeleBot(TOKEN)
+
+value = ''
+old_value = ''
+
+keyboard = telebot.types.InlineKeyboardMarkup()
+keyboard.row(telebot.types.InlineKeyboardButton(' ', callback_data='no'),
+             telebot.types.InlineKeyboardButton('C', callback_data='C'),
+             telebot.types.InlineKeyboardButton('<=', callback_data='<='),
+             telebot.types.InlineKeyboardButton('/', callback_data='/'))
+keyboard.row(telebot.types.InlineKeyboardButton('7', callback_data='7'),
+             telebot.types.InlineKeyboardButton('8', callback_data='8'),
+             telebot.types.InlineKeyboardButton('9', callback_data='9'),
+             telebot.types.InlineKeyboardButton('*', callback_data='*'))
+keyboard.row(telebot.types.InlineKeyboardButton('4', callback_data='4'),
+             telebot.types.InlineKeyboardButton('5', callback_data='5'),
+             telebot.types.InlineKeyboardButton('6', callback_data='6'),
+             telebot.types.InlineKeyboardButton('-', callback_data='-'))
+keyboard.row(telebot.types.InlineKeyboardButton('1', callback_data='1'),
+             telebot.types.InlineKeyboardButton('2', callback_data='2'),
+             telebot.types.InlineKeyboardButton('3', callback_data='3'),
+             telebot.types.InlineKeyboardButton('+', callback_data='+'))
+keyboard.row(telebot.types.InlineKeyboardButton(' ', callback_data='no'),
+             telebot.types.InlineKeyboardButton('0', callback_data='0'),
+             telebot.types.InlineKeyboardButton(',', callback_data='.'),
+             telebot.types.InlineKeyboardButton('=', callback_data='='))
+
+
+@bot.message_handler(commands=['calc'])
+def getMessage(message):
+    global value
+    if value == '':
+        bot.send_message(message.from_user.id,
+                         '0', reply_markup=keyboard)
+    else:
+        bot.send_message(message.from_user.id,
+                         value, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_func(query):
+    global value, old_value
+    data = query.data
+    if data == 'no':
+        pass
+    elif data == 'C':
+        value = ''
+    elif data == '<=':
+        if value != '':
+            value = value[:len(value)-1]
+    elif data == '=':
+        value = str(eval(value))
+    else:
+        value += data
+    if value != old_value:
+        if value == '':
+            bot.edit_message_text(
+                chat_id=query.message.chat.id, message_id=query.message.id, text='0', reply_markup=keyboard)
+        else:
+            bot.edit_message_text(chat_id=query.message.chat.id,
+                                  message_id=query.message.id, text=value, reply_markup=keyboard)
+        old_value = value
+
+
+bot.polling()
 
